3680d75b0e8aae25f21bb92de9e7e22df5069332
Brama Udi <bramaudi.al@gmail.com>
2023-02-16 20:59:34 +0700

    Add link to go original post

--- src/pages/item/[id].tsx
+++ src/pages/item/[id].tsx
@@ -28,7 +28,7 @@ export default function () {
                     <Show when={item()!.read}>[<a onClick={() => markAsUnread(item()!.id)}>Mark as unread</a>]</Show>
                 </div>
                 <article>
-                    <h1>{item()!.title}</h1>
+                    <h1>{item()!.title} <a href={item()?.link} title="Original website">&#128279;</a></h1>
                     <span class="meta">
                         {item()!.author && `by ${item()!.author?.name}, `}
                         {formatDate(item()!.lastModified)}
