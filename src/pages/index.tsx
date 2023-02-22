e3a9ac122cb4e0c3037192e4d39b66f6313f39ae
Brama Udi <bramaudi.al@gmail.com>
2023-02-22 16:21:45 +0700

    fix typo and adjust

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,10 +2,10 @@ export default function () {
     return (
         <>
             <h2>A super simple feed reader just using your browser</h2>
-            <h3>No singup, No Ads, No fancy UI, Just read.</h3>
+            <h3>No signup, No Ads, No fancy UI, Just read.</h3>
 
             <p>
-                Read your favorite news, personal blog, or anything else. <br />
+                Read your favorite news, personal blog content, or anything else. <br />
                 Literally everything as long as they provide
                 RSS/Atom feed.
             </p>
@@ -15,11 +15,6 @@ export default function () {
                 So becareful before clear your browser web storage ;)
             </p>
 
-            <p>
-                We also using our own proxy to fetch feed and converting to API
-                which is open source.
-            </p>
-
             <blockquote>
                 <strong>Warning</strong>: Still under development, some functionality may break without any caution,
                 and lacks of many basic features.
@@ -27,6 +22,7 @@ export default function () {
 
             <a href="https://github.com/bramaudi/nanorss">Source code</a> | 
             <a href="https://github.com/bramaudi/rss2json">API</a>
+            
             <footer style="padding:25px 0;">
                 Inspired by <a href="https://bearblog.dev">Bear ʕ•ᴥ•ʔ</a>
             </footer>
